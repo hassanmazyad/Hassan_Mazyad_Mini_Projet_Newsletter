3545d5e6ff2da59f203e022abf1b794213968e18
Hassan Mazyad <hassan.mazyad@outlook.com>
2020-11-22 22:25:22 +0100

    create FavoriteFragment

---
+++ app/src/main/java/com/example/newsletter/fragments/FavoriteFragment.kt
@@ -0,0 +1,39 @@
+package com.example.newsletter.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsletter.R
+import com.example.newsletter.adapters.ArticleAdapter
+import com.example.newsletter.data.Article
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FavoriteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FavoriteFragment : Fragment() {
+    lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_articles, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        val articlesFavorites = listOf<Article>()
+        val adapterRecycler = ArticleAdapter(articlesFavorites)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+        recyclerView.adapter = adapterRecycler
+    }
+
+}
